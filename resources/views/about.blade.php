ecb9a0faa9beb79b165697ee6b0423dff36c316e
BVD <Lcjsleeper@gmail.com>
2025-04-07 15:03:47 +0200

    Caca

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Qui somme nous?</title>
+</head>
+<body>
+    <h1>Nous somme Valabas</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
+Route::get('/Contact', function () {return view('contact');});
+Route::get('/About', function () {return view('Qui somme nous?');});
