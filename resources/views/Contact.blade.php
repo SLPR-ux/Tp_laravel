106ffbbee9e660356610969687d38c59329c2345
BVD <Lcjsleeper@gmail.com>
2025-04-07 17:11:50 +0200

    menu-app-footer

--- resources/views/Contact.blade.php
+++ resources/views/Contact.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>contactez nous?</title>
-</head>
-<body>
-    <x-menu />
-    <h1>appel moi bg</h1>
-    <x-footer />
-</body>
-</html>
+<x-app>
+    <h1>Nous contacter?</h1>
+    
+
+</x-app>
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Qui somme nous?</title>
-</head>
-<body>
-    <x-menu />
-    <h1>Nous somme Valabas</h1>
-    <x-footer />
-</body>
-</html>
+<x-app>
+    <h1>si on parle de nous de nous parlation de nous nous meme?</h1>
+</x-app>
